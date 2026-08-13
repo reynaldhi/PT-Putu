94378d14ad939b3e723c6758cd2c0f6625e8b273
Reynaldhi <reynaldhibagas12@gmail.com>
2026-08-13 22:27:07 +0700

    feat: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,12 @@ export const metadata: Metadata = {
   title: "PT Putu Marta Mandiri - Engineering Precision & Modern Design Solutions",
   description: "Specializing in heavy-duty industrial gas piping and refined commercial interior design. We deliver structural integrity and aesthetic excellence.",
   keywords: ["Gas Piping", "Industrial Gas Piping", "Interior Design", "Kontraktor Interior", "PT Putu Marta Mandiri", "Sertifikasi K3"],
+
+  icons: {
+    icon: "/images/logo/logo logo-pmm-02.png",
+    shortcut: "/images/logo/logo logo-pmm-02.png",
+    apple: "/images/logo/logo logo-pmm-02.png",
+  },
 };
 
 export default function RootLayout({
